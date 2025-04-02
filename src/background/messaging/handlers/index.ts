33f164d625fc88e66526b91961b021a4c8bb4620
Xingyu Wang <xingyu.wang@gmail.com>
2025-04-02 17:43:24 +0800

    清理过期的标签页处理程序

--- src/background/messaging/handlers/index.ts
+++ src/background/messaging/handlers/index.ts
@@ -1,16 +1,2 @@
-import { Logger } from '../../../lib/utils/logger.js';
-import * as tabHandlers from './tab.js';
-import { BackgroundMessageService } from '../bg-message-service.js';
-
-const logger = new Logger('TabHandlers');
-// 导出所有处理程序
-export { tabHandlers };
-
-/**
- * 注册所有标签页相关处理程序
- */
-export function registerTabHandlers(messageService: BackgroundMessageService): void {
-  messageService.registerHandler('getTabId', tabHandlers.getTabId);
-  
-  logger.log('标签页相关消息处理程序已注册');
-}
+export { registerTabHandlers } from './tab-handlers.js';
+export { registerSettingsHandlers } from './settings-handlers.js';
